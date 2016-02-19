41cef4b89502d5b2ff57041a8e3c94cb4ed95b00
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-02-20 00:06:44 +0530

    Initial Commit

---
+++ TableViewCellIndexingExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewCellIndexingExample
+//
+//  Created by Maitrayee Ghosh on 20/02/16.
+//  Copyright © 2016 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
